b3528752c4f54ac7dba82348eaf2c2e203a58f22
Fabrício Zimmerer Murta <fabricio@object.net>
2018-08-13 15:47:45 -0300

    Adds CDN files/resources for Ext.NET 4.7.0.

---
+++ ext.net/4.7.0/extnet/extnet-charts-debug.js
@@ -0,0 +1,161 @@
+/*
+ * @version   : 4.7.0 - Ext.NET License
+ * @author    : Object.NET, Inc. http://object.net/
+ * @date      : 2018-08-13
+ * @copyright : Copyright (c) 2008-2018, Object.NET, Inc. (http://object.net/). All rights reserved.
+ * @license   : See license.txt and http://ext.net/license/.
+ */
+
+
+// @source charts/legend/SpriteLegend.js
+
+Ext.define('gh1543', {
+    override: 'Ext.chart.legend.SpriteLegend',
+    performLayout: function () {
+        var me = this,
+            sprites = me.getSprites(),
+            surface = me.getSurface(),
+            surfaceRect = surface.getRect(),
+            // gap is 4x for the theme-driven borders
+            gap = 4 * me.getPadding(),
+            i, sprite, bbox, rec, name;
+
+        if (!surface || !surfaceRect) {
+            return false;
+        }
+
+        var docked = me.getDocked(),
+            surfaceWidth = surfaceRect[2],
+            legendMaxWidth = surfaceWidth - gap,
+            surfaceHeight = surfaceRect[3],
+            bboxes = [];
+
+        Ext.each(sprites, function (sprite) {
+            bboxes.push(sprite.getBBox());
+        })
+
+        switch (docked) {
+            case 'bottom':
+            case 'top':
+                if (!surfaceWidth) {
+                    return false;
+                }
+
+                // Parses every legend sprite, cropping text until it fits the current chart
+                // canvas space.
+                for (i = 0; i < bboxes.length; i++) {
+                    bbox = bboxes[i];
+                    sprite = sprites[i];
+
+                    while (bbox.width > legendMaxWidth) {
+                        rec = sprite.getRecord();
+                        name = rec.get('name');
+                        name = name.substr(0, name.length - 1);
+                        rec.set('name', name);
+
+                        // Reduced the record string to a minimum yet it does not fit.
+                        if (name.length < 1) {
+                            Ext.Error.raise("Chart is way too narrow to draw any legend text at all.");
+                        }
+
+                        sprite.getBBox();
+                    }
+                }
+
+                break;
+        }
+
+        return me.callParent(arguments);
+    }
+});
+
+Ext.define('gh1606', {
+    override: 'Ext.chart.legend.SpriteLegend',
+    isXType: function () {
+        // This does not have a xtype definition, so always return false.
+        return false;
+    }
+});
+// @source charts/series/cartesian.js
+Ext.define('gh1486', {
+    override: 'Ext.chart.series.Cartesian',
+
+    // This will enable data gaps handling, implemented by issue #1486.
+    dataGapsHandling: false,
+
+    getYRange: function () {
+        var me = this;
+
+        if (me.dataGapsHandling) {
+            return (me.dataRange[1] === null || me.dataRange[3] === null)
+                ? null : [me.dataRange[1], me.dataRange[3]];
+        } else {
+            return me.callParent(arguments);
+        }
+        
+    },
+    getRangeOfData: function (data, range) {
+        var me = this;
+
+        if (me.dataGapsHandling) {
+            var i, length = data.length,
+                value, min = range.min, max = range.max;
+
+            for (i = 0; i < length; i++) {
+                value = data[i];
+
+                if (Ext.isNumeric(value)) {
+                    if (value < min) {
+                        min = value;
+                    }
+                    if (value > max) {
+                        max = value;
+                    }
+                }
+            }
+
+            range.min = min;
+            range.max = max;
+        } else {
+            return me.callParent(arguments);
+        }
+    },
+    coordinateData: function (items, field, axis) {
+        var me = this;
+
+        if (me.dataGapsHandling) {
+            var data = this.callParent(arguments),
+                lim = data.length,
+                i;
+
+            for (i = 0; i < lim; i++) {
+                if (!Ext.isNumeric(data[i])) {
+                    data[i] = NaN;
+                }
+            }
+
+            return data;
+        } else {
+            return me.callParent(arguments);
+        }
+    }
+});
+// @source charts/series/cartesian.js
+Ext.define('gh1497', {
+    override: 'Ext.chart.series.Pie3D',
+    coordinateX: function () {
+        var me = this,
+            retVal = me.callParent(arguments),
+            chart = me.getChart();
+
+        // 'refreshLegendStore' will attemp to grab the 'series',
+        // which are still configuring at this point.
+        // The legend store will be refreshed inside the chart.series
+        // updater anyway.
+        if (!chart.isConfiguring) {
+            chart.refreshLegendStore();
+        };
+
+        return retVal;
+    }
+});
---
+++ ext.net/4.7.0/ux/toolbardroppable/ToolbarDroppable-merged-debug.js
@@ -0,0 +1,212 @@
+
+ Ext.define('Ext.ux.ToolbarDroppable', {
+
+    
+    constructor: function(config) {
+      Ext.apply(this, config);
+    },
+
+    
+    init: function(toolbar) {
+      
+      this.toolbar = toolbar;
+
+      this.toolbar.on({
+          scope : this,
+          render: this.createDropTarget
+      });
+    },
+
+    
+    createDropTarget: function() {
+        
+        this.dropTarget = Ext.create('Ext.dd.DropTarget', this.toolbar.getEl(), {
+            notifyOver: this.notifyOver.bind(this),
+            notifyDrop: this.notifyDrop.bind(this)
+        });
+    },
+
+    
+    addDDGroup: function(ddGroup) {
+        this.dropTarget.addToGroup(ddGroup);
+    },
+
+    
+    calculateEntryIndex: function(e) {
+        var entryIndex = 0,
+            toolbar = this.toolbar,
+            items = toolbar.items.items,
+            count = items.length,
+            xHover = e.getXY()[0],
+            index = 0,
+            el, xTotal, width, midpoint;
+
+        for (; index < count; index++) {
+            el = items[index].getEl();
+            xTotal = el.getXY()[0];
+            width = el.getWidth();
+            midpoint = xTotal + width / 2;
+
+            if (xHover < midpoint) {
+                entryIndex = index;
+                break;
+            } else {
+                entryIndex = index + 1;
+            }
+       }
+       return entryIndex;
+    },
+
+    
+    canDrop: function(data) {
+        return true;
+    },
+
+    
+    notifyOver: function(dragSource, event, data) {
+        return this.canDrop.apply(this, arguments) ? this.dropTarget.dropAllowed : this.dropTarget.dropNotAllowed;
+    },
+
+    
+    notifyDrop: function(dragSource, event, data) {
+        var canAdd = this.canDrop(dragSource, event, data),
+            tbar   = this.toolbar;
+
+        if (canAdd) {
+            var entryIndex = this.calculateEntryIndex(event);
+
+            tbar.insert(entryIndex, this.createItem(data));
+
+            this.afterLayout();
+        }
+
+        return canAdd;
+    },
+
+    
+    createItem: function(data) {
+        //<debug>
+        Ext.raise("The createItem method must be implemented in the ToolbarDroppable plugin");
+        //</debug>
+    },
+
+    
+    afterLayout: Ext.emptyFn
+});
+
+// @source: toolbardroppable/ToolbarDroppable-overrides.js
+Ext.define('Ext.ux.ToolbarDroppable', {
+    override: 'Ext.ux.ToolbarDroppable',
+
+    mixins: ['Ext.mixin.Observable'],
+
+    constructor: function (config) {
+        var me = this
+        canDrop = undefined;
+
+        // Method argument passed as config option does not work as
+        // it tries to override the instance in place. To overcome
+        // this, we leave it out of the constructor config parameters
+        // and then replace the method when it has been instantiated.
+        if (config.canDrop) {
+            canDrop = config.canDrop;
+            delete config.canDrop;
+        }
+
+        me.callParent(arguments);
+
+        // Observable mixin cannot apply the createItem function. So let
+        // it run the default constructor to replace it, then remove the
+        // reference to createItem from the constructor's config before
+        // calling the Observable constructor.
+        if (config.createItem) {
+            delete config.createItem;
+        };
+        me.mixins.observable.constructor.call(me, config);
+
+        // If canDrop was saved above, then replace whatever is set as
+        // canDrop method in the instance.
+        if (canDrop !== undefined) {
+            me.canDrop = canDrop;
+        }
+    },
+
+    notifyDrop: function (dragSource, event, data) {
+        var me = this,
+            canAdd = me.canDrop(dragSource, event, data),
+            tbar = me.toolbar,
+            retVal;
+
+        if (canAdd) {
+            if (me.remote) {
+                var entryIndex = me.calculateEntryIndex(event),
+                    remoteOptions = { index: entryIndex },
+                    dc = me.directEventConfig || {},
+                    loadingItem;
+
+                if (me.fireEvent("beforeremotecreate", me, data, remoteOptions, dragSource, event) === false) {
+                    return false;
+                }
+
+                loadingItem = new Ext.toolbar.TextItem({
+                    text: "<div class='x-loading-indicator' style='width:16px;'>&nbsp;</div>"
+                });
+                tbar.insert(entryIndex, loadingItem);
+
+                dc.userSuccess = Ext.Function.bind(me.remoteCreateSuccess, me);
+                dc.userFailure = Ext.Function.bind(me.remoteCreateFailure, me);
+                dc.extraParams = remoteOptions;
+                dc.control = me;
+                dc.entryIndex = entryIndex;
+                dc._data = data;
+                dc.loadingItem = loadingItem;
+                dc.eventType = "postback";
+                dc.action = "create";
+
+                Ext.net.DirectEvent.request(dc);
+
+                me.afterLayout();
+            }
+            else {
+                retVal = me.callParent(arguments);
+            }
+        }
+
+        return retVal || canAdd;
+    },
+
+    remoteCreateSuccess: function (response, result, context, type, action, extraParams, o) {
+        this.toolbar.remove(o.loadingItem);
+
+        var rParams,
+            entryIndex,
+            item;
+
+        try {
+            rParams = result.extraParamsResponse || {};
+            var responseObj = result.serviceResponse;
+            result = { success: responseObj.success, msg: responseObj.message };
+        } catch (ex) {
+            result.success = false;
+            result.msg = ex.message;
+        }
+
+        this.fireEvent("remotecreate", this, !!result.success, result.msg, response, o);
+
+        entryIndex = Ext.isDefined(rParams.ra_index) ? rParams.ra_index : o.entryIndex;
+        item = Ext.decode(rParams.ra_item);
+        this.toolbar.insert(entryIndex, item);
+        this.fireEvent("drop", this, item, entryIndex, o._data);
+
+        this.toolbar.updateLayout();
+        this.afterLayout();
+    },
+
+    remoteCreateFailure: function (response, result, context, type, action, extraParams, o) {
+        this.toolbar.remove(o.loadingItem);
+        this.fireEvent("remotecreate", this, false, response.responseText, response, o);
+
+        this.toolbar.updateLayout();
+        this.afterLayout();
+    }
+});
