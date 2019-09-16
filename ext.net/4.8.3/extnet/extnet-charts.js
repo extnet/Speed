bc1d735dd61cb234f3a6bcff96266ff6768da905
Fabrício Zimmerer Murta <fabricio@object.net>
2019-09-16 05:16:34 -0300

    Adds CDN files/resources for Ext.NET 4.8.3.

---
+++ ext.net/4.8.3/extnet/extnet-charts.js
@@ -0,0 +1,23 @@
+/*
+ * @version   : 4.8.3 - Ext.NET License
+ * @author    : Object.NET, Inc. https://object.net/
+ * @date      : 2019-08-27
+ * @copyright : Copyright (c) 2008-2019, Object.NET, Inc. (https://object.net/). All rights reserved.
+ * @license   : See license.txt and https://ext.net/license/.
+ */
+
+
+Ext.define('gh1543',{override:'Ext.chart.legend.SpriteLegend',performLayout:function(){var me=this,sprites=me.getSprites(),surface=me.getSurface(),surfaceRect=surface.getRect(),gap=4*me.getPadding(),i,sprite,bbox,rec,name;if(!surface||!surfaceRect){return false;}
+var docked=me.getDocked(),surfaceWidth=surfaceRect[2],legendMaxWidth=surfaceWidth-gap,surfaceHeight=surfaceRect[3],bboxes=[];Ext.each(sprites,function(sprite){bboxes.push(sprite.getBBox());})
+switch(docked){case'bottom':case'top':if(!surfaceWidth){return false;}
+for(i=0;i<bboxes.length;i++){bbox=bboxes[i];sprite=sprites[i];while(bbox.width>legendMaxWidth){rec=sprite.getRecord();name=rec.get('name');name=name.substr(0,name.length-1);rec.set('name',name);if(name.length<1){Ext.Error.raise("Chart is way too narrow to draw any legend text at all.");}
+sprite.getBBox();}}
+break;}
+return me.callParent(arguments);}});Ext.define('gh1606',{override:'Ext.chart.legend.SpriteLegend',isXType:function(){return false;}});
+
+Ext.define('gh1486',{override:'Ext.chart.series.Cartesian',dataGapsHandling:false,getYRange:function(){var me=this;if(me.dataGapsHandling){return(me.dataRange[1]===null||me.dataRange[3]===null)?null:[me.dataRange[1],me.dataRange[3]];}else{return me.callParent(arguments);}},getRangeOfData:function(data,range){var me=this;if(me.dataGapsHandling){var i,length=data.length,value,min=range.min,max=range.max;for(i=0;i<length;i++){value=data[i];if(Ext.isNumeric(value)){if(value<min){min=value;}
+if(value>max){max=value;}}}
+range.min=min;range.max=max;}else{return me.callParent(arguments);}},coordinateData:function(items,field,axis){var me=this;if(me.dataGapsHandling){var data=this.callParent(arguments),lim=data.length,i;for(i=0;i<lim;i++){if(!Ext.isNumeric(data[i])){data[i]=NaN;}}
+return data;}else{return me.callParent(arguments);}}});
+
+Ext.define('gh1497',{override:'Ext.chart.series.Pie3D',coordinateX:function(){var me=this,retVal=me.callParent(arguments),chart=me.getChart();if(!chart.isConfiguring){chart.refreshLegendStore();};return retVal;}});
---
+++ ext.net/4.8.3/ux/dataview/DragSelector-debug.js
@@ -0,0 +1,200 @@
+
+Ext.define('Ext.ux.DataView.DragSelector', {
+    requires: ['Ext.dd.DragTracker', 'Ext.util.Region'],
+    alias: 'plugin.dataviewdragselector',
+
+    
+    init: function(dataview) {
+        var scroller = dataview.getScrollable();
+
+        // If the client dataview is scrollable, and this is a PointerEvents device
+        // we cannot intercept the pointer to inplement dragselect.
+        if (scroller && (scroller.getX() || scroller.getY()) &&
+            (Ext.supports.PointerEvents || Ext.supports.MSPointerEvents)) {
+            //<debug>
+            Ext.log.warn('DragSelector not available on PointerEvent devices');
+            //</debug>
+
+            return;
+        }
+
+        
+        this.dataview = dataview;
+
+        dataview.mon(dataview, {
+            beforecontainerclick: this.cancelClick,
+            scope: this,
+            render: {
+                fn: this.onRender,
+                scope: this,
+                single: true
+            }
+        });
+    },
+
+    
+    onRender: function() {
+        
+        this.tracker = Ext.create('Ext.dd.DragTracker', {
+            dataview: this.dataview,
+            el: this.dataview.el,
+            onBeforeStart: this.onBeforeStart,
+            onStart: this.onStart.bind(this),
+            onDrag: this.onDrag.bind(this),
+            onEnd: Ext.Function.createDelayed(this.onEnd, 100, this)
+        });
+
+        
+        this.dragRegion = Ext.create('Ext.util.Region');
+    },
+
+    
+    onBeforeStart: function(e) {
+        return e.target === this.dataview.getEl().dom;
+    },
+
+    
+    onStart: function(e) {
+        var dataview = this.dataview;
+
+        // Flag which controls whether the cancelClick method vetoes the processing of the
+        // DataView's containerclick event.
+        // On IE (where else), this needs to remain set for a millisecond after mouseup because
+        // even though the mouse has moved, the mouseup will still trigger a click event.
+        this.dragging = true;
+
+        // here we reset and show the selection proxy element and cache the regions each item
+        // in the dataview take up
+        this.fillRegions();
+        this.getProxy().show();
+        dataview.getSelectionModel().deselectAll();
+    },
+
+    
+    cancelClick: function() {
+        return !this.dragging;
+    },
+
+    
+    onDrag: function(e) {
+        var selModel = this.dataview.getSelectionModel(),
+            dragRegion = this.dragRegion,
+            bodyRegion = this.bodyRegion,
+            proxy = this.getProxy(),
+            regions = this.regions,
+            length = regions.length,
+            startXY = this.tracker.startXY,
+            currentXY = this.tracker.getXY(),
+            minX = Math.min(startXY[0], currentXY[0]),
+            minY = Math.min(startXY[1], currentXY[1]),
+            width = Math.abs(startXY[0] - currentXY[0]),
+            height = Math.abs(startXY[1] - currentXY[1]),
+            region, selected, i;
+
+        Ext.apply(dragRegion, {
+            top: minY,
+            left: minX,
+            right: minX + width,
+            bottom: minY + height
+        });
+
+        dragRegion.constrainTo(bodyRegion);
+        proxy.setBox(dragRegion);
+
+        for (i = 0; i < length; i++) {
+            region = regions[i];
+            selected = dragRegion.intersect(region);
+
+            if (selected) {
+                selModel.select(i, true);
+            }
+            else {
+                selModel.deselect(i);
+            }
+        }
+    },
+
+    
+    onEnd: function(e) {
+        var dataview = this.dataview,
+            selModel = dataview.getSelectionModel(); // eslint-disable-line no-unused-vars
+
+        this.dragging = false;
+        this.getProxy().hide();
+    },
+
+    
+    getProxy: function() {
+        if (!this.proxy) {
+            this.proxy = this.dataview.getEl().createChild({
+                tag: 'div',
+                cls: 'x-view-selector'
+            });
+        }
+
+        return this.proxy;
+    },
+
+    
+    fillRegions: function() {
+        var dataview = this.dataview,
+            regions = this.regions = [];
+
+        dataview.all.each(function(node) {
+            regions.push(node.getRegion());
+        });
+        this.bodyRegion = dataview.getEl().getRegion();
+    }
+});
+
+// @source: dataview/overrides/DragSelector.js
+Ext.define('Ext.ux.DataView.DragSelector', {
+    override: 'Ext.ux.DataView.DragSelector',
+
+    // Respects the browsers that implement PointerEvents if the data view is
+    // scrollable (not enabling the drag selector at all in that case).
+    // This is per this w3c recommendation: http://www.w3.org/TR/pointerevents/
+    // And this option lets the user choose whether to obey it or not.
+    respectPointerEvents: true,
+
+    constructor: function (config) {
+        var me = this;
+        Ext.apply(me, config);
+        me.callParent(arguments);
+    },
+
+    init: function (dataview) {
+        var me = this,
+            scroller = dataview.getScrollable();
+
+        // Call original constructor
+        me.callParent(arguments);
+
+        // If the condition here is true, it should mean the original
+        // constructor has returned earlier on PointerEvents devices,
+        // not enabling the DragSelector functionality at all.
+        if (scroller && (scroller.getX() || scroller.getY()) && (Ext.supports.PointerEvents || Ext.supports.MSPointerEvents)) {
+            // Do not return if we are not respecting pointerEvents-capable browsers
+            if (me.respectPointerEvents) {
+                //<debug>
+                Ext.log.warn('DragSelector not available on PointerEvent devices')
+                //</debug>
+                return;
+            }
+        }
+
+        // This will run when either:
+        // - DataView is not scrollable
+        // - DataView is scrollable and browser is not PointerEvents-driven.
+        this.dataview = dataview;
+        dataview.mon(dataview, {
+            beforecontainerclick: this.cancelClick,
+            scope: this,
+            render: {
+                fn: this.onRender,
+                scope: this,
+                single: true
+            }
+        });
+    }
+});
