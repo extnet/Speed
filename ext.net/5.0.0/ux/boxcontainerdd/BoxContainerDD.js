a14ab8a3e912795b1ef259ec2b2d8f2afc447183
Fabrício Zimmerer Murta <fabricio@object.net>
2019-09-16 05:17:03 -0300

    Adds CDN files/resources for Ext.NET 5.0.0.

---
+++ ext.net/5.0.0/ux/boxcontainerdd/BoxContainerDD.js
@@ -0,0 +1,12 @@
+/*
+ * @version   : 5.0.0 - Ext.NET License
+ * @author    : Object.NET, Inc. https://object.net/
+ * @date      : 2019-09-16
+ * @copyright : Copyright (c) 2008-2019, Object.NET, Inc. (https://object.net/). All rights reserved.
+ * @license   : See license.txt and https://ext.net/license/
+ */
+
+
+Ext.define('Ext.ux.dd.BoxContainerDD',{extend:'Ext.dd.DD',alignElWithMouse:function(el,iPageX,iPageY){var me=this,oCoord=me.getTargetCoord(iPageX,iPageY),x=oCoord.x,y=oCoord.y,fly=el.dom?el:Ext.fly(el,'_dd'),aCoord,newLeft,newTop;if(!me.deltaSetXY){aCoord=[Math.max(0,x),Math.max(0,y)];fly.setXY(aCoord);newLeft=me.getLocalX(fly);newTop=fly.getLocalY();me.deltaSetXY=[newLeft-x,newTop-y];}
+else{me.setLocalXY(fly,Math.max(0,x+me.deltaSetXY[0]),Math.max(0,y+me.deltaSetXY[1]));}
+me.cachePosition(x,y);me.autoScroll(x,y,el.offsetHeight,el.offsetWidth);return oCoord;}});
---
+++ ext.net/5.0.0/ux/tabclosemenu/TabCloseMenu.js
@@ -0,0 +1,27 @@
+/*
+ * @version   : 5.0.0 - Ext.NET License
+ * @author    : Object.NET, Inc. https://object.net/
+ * @date      : 2019-09-16
+ * @copyright : Copyright (c) 2008-2019, Object.NET, Inc. (https://object.net/). All rights reserved.
+ * @license   : See license.txt and https://ext.net/license/
+ */
+
+
+Ext.define('Ext.ux.TabCloseMenu',{extend:'Ext.plugin.Abstract',alias:'plugin.tabclosemenu',mixins:{observable:'Ext.util.Observable'},closeTabText:'Close Tab',showCloseOthers:true,closeOtherTabsText:'Close Other Tabs',showCloseAll:true,closeAllTabsText:'Close All Tabs',extraItemsHead:null,extraItemsTail:null,constructor:function(config){this.callParent([config]);this.mixins.observable.constructor.call(this,config);},init:function(tabpanel){this.tabPanel=tabpanel;this.tabBar=tabpanel.down("tabbar");this.mon(this.tabPanel,{scope:this,afterlayout:this.onAfterLayout,single:true});},onAfterLayout:function(){this.mon(this.tabBar.el,{scope:this,contextmenu:this.onContextMenu,delegate:'.x-tab'});},destroy:function(){Ext.destroy(this.menu);this.callParent();},onContextMenu:function(event,target){var me=this,menu=me.createMenu(),disableAll=true,disableOthers=true,tab=me.tabBar.getChildByElement(target),index=me.tabBar.items.indexOf(tab);me.item=me.tabPanel.getComponent(index);menu.child('#close').setDisabled(!me.item.closable);if(me.showCloseAll||me.showCloseOthers){me.tabPanel.items.each(function(item){if(item.closable){disableAll=false;if(item!==me.item){disableOthers=false;return false;}}
+return true;});if(me.showCloseAll){menu.child('#closeAll').setDisabled(disableAll);}
+if(me.showCloseOthers){menu.child('#closeOthers').setDisabled(disableOthers);}}
+event.preventDefault();me.fireEvent('beforemenu',menu,me.item,me);menu.showAt(event.getXY());},createMenu:function(){var me=this,items;if(!me.menu){items=[{itemId:'close',text:me.closeTabText,scope:me,handler:me.onClose}];if(me.showCloseAll||me.showCloseOthers){items.push('-');}
+if(me.showCloseOthers){items.push({itemId:'closeOthers',text:me.closeOtherTabsText,scope:me,handler:me.onCloseOthers});}
+if(me.showCloseAll){items.push({itemId:'closeAll',text:me.closeAllTabsText,scope:me,handler:me.onCloseAll});}
+if(me.extraItemsHead){items=me.extraItemsHead.concat(items);}
+if(me.extraItemsTail){items=items.concat(me.extraItemsTail);}
+me.menu=Ext.create('Ext.menu.Menu',{items:items,listeners:{hide:me.onHideMenu,scope:me}});}
+return me.menu;},onHideMenu:function(){var me=this;me.fireEvent('aftermenu',me.menu,me);},onClose:function(){this.tabPanel.remove(this.item);},onCloseOthers:function(){this.doClose(true);},onCloseAll:function(){this.doClose(false);},doClose:function(excludeActive){var items=[];this.tabPanel.items.each(function(item){if(item.closable){if(!excludeActive||item!==this.item){items.push(item);}}},this);Ext.suspendLayouts();Ext.Array.forEach(items,function(item){this.tabPanel.remove(item);},this);Ext.resumeLayouts(true);}});
+
+Ext.define('gh1648',{override:'Ext.ux.TabCloseMenu',closeTabIconCls:null,closeOtherTabsIconCls:null,closeAllTabsIconCls:null,createMenu:function(){var me=this,items;if(!me.menu){items=[{itemId:'close',text:me.closeTabText,iconCls:me.closeTabIconCls,scope:me,handler:me.onClose}];if(me.showCloseAll||me.showCloseOthers){items.push('-');}
+if(me.showCloseOthers){items.push({itemId:'closeOthers',text:me.closeOthersTabsText,iconCls:me.closeOtherTabsIconCls,scope:me,handler:me.onCloseOthers});}
+if(me.showCloseAll){items.push({itemId:'closeAll',text:me.closeAllTabsText,iconCls:me.closeAllTabsIconCls,scope:me,handler:me.onCloseAll});}
+if(me.extraItemsHead){items=me.extraItemsHead.concat(items);}
+if(me.extraItemsTail){items=items.concat(me.extraItemsTail);}
+me.menu=Ext.create('Ext.menu.Menu',{items:items,listeners:{hide:me.onHideMenu,scope:me}});}
+return me.menu;},onHideMenu:function(){var me=this;me.item=null;me.fireEvent('aftermenu',me.menu,me);},onClose:function(){this.tabPanel.closeTab(this.item);},doClose:function(excludeActive){var items=[];this.tabPanel.items.each(function(item){if(item.closable){if(!excludeActive||item!==this.item){items.push(item);}}},this);Ext.suspendLayouts();Ext.Array.forEach(items,function(item){this.tabPanel.closeTab(item);},this);Ext.resumeLayouts(true);}});
